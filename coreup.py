37184eb65742b4911d438180e7f348e135435cf8
AsP3X <development@vorberg-webmail.de>
2023-02-09 06:04:33 +0000

    TASK: started rewrite of main

--- coreup.py
+++ coreup.py
@@ -6,13 +6,14 @@ import json
 from assets.logging import Logger
 from assets.configManager import ConfigManager
 from assets.timeCalc import TimeCalculator
+from assets.timecoderunner import TimingCodeRunner
 
 # Setup Config Manager
 configmngr = ConfigManager("configs/config.json")
 config = configmngr.readConfig()
 
 # Setup logger
-logger = Logger("coreup", "./logs", "log")
+logger = Logger("COREUP", "./logs", "log")
 
 # Setup TimeCalculator
 timec = TimeCalculator()
@@ -30,18 +31,8 @@ def checkFolders(path):
         logger.info(f"Creating directory tree at: {path}")
         os.makedirs(path)
 
-def checkForSources(path):
-    tasks = []
-
-    for file in os.listdir(path):
-        d = os.path.join(path, file)
-        if os.path.isdir(d):
-            task = d.rsplit('/', 1)[-1]
-            tasks.append(task)
-
-    return tasks
-
 def checkForConfig(path, project):
+    if not os.path.isdir(path): return False
     configList = os.listdir(path)
     if f"{project}.json" in configList:
         return True
@@ -57,8 +48,7 @@ def getBConfig(path):
 class ContinuousLoop:
     def __init__(self, interval=interval):
         self.script_root = os.getcwd()
-        self.backup_path = f"{self.script_root}/backups/"
-        self.backup_configs = f"{self.script_root}/configs/backups"
+        self.backup_configs = f"{self.script_root}/configs/tasks"
         self.interval = interval
         self.tcdata = timec.convert_to_dhms(interval)
         self.days = self.tcdata["days"] if self.tcdata["days"] else 0
@@ -66,17 +56,15 @@ class ContinuousLoop:
         self.minutes = self.tcdata["minutes"] if self.tcdata["minutes"] else 0
         self.seconds = self.tcdata["seconds"] if self.tcdata["seconds"] else 0
 
+        self.tasks = []
+
         logger.log("Starting backup service")
         logger.log(f"Current Version: {version} from {release}")
         logger.info(f"current interval is set to {self.days}d {self.hours}h {self.minutes}m {self.seconds}s")
         logger.info(f"Script root: {self.script_root}")
-        logger.info(f"backup path: {self.backup_path}")
         logger.info(f"backup configs: {self.backup_configs}")
 
         checkFolders("configs/backups")
-        checkFolders("backups")
-        checkFolders("backups/source")
-        checkFolders("backups/out")
 
     def run(self):
         while True:
@@ -85,8 +73,22 @@ class ContinuousLoop:
 
     def loop(self):
         logger.log("Running check...")
-        tasks = checkForSources(f"{self.backup_path}/source")
-        logger.info(checkForConfig(self.backup_configs, tasks[0]))
+        
+        if not os.path.isdir(self.backup_configs):
+            logger.warning(f"No Tasks found, please create a new Task in: {self.backup_configs}")
+            return
+
+        for task in os.listdir(self.backup_configs):
+            task_name = task.split('.')[0]
+            
+            if not task_name in self.tasks:
+                self.tasks.append(task_name)
+
+
+
+        # if not tasks == []:
+        #     logger.info(checkForConfig(self.backup_configs, tasks[0]))
+        
         logger.info(f"Next check in {self.days}d {self.hours}h {self.minutes}m {self.seconds}s")
             
 if __name__ == "__main__":
