4dba8ee15c60190ff4a3a35f8a94e23d47a4d928
AsP3X <development@vorberg-webmail.de>
2023-02-09 09:39:25 +0000

    TASK: refactored code to support sameline output

--- assets/logging.py
+++ assets/logging.py
@@ -11,32 +11,38 @@ class Logger:
         if not os.path.exists(self.logPath):
             os.makedirs(self.logPath)
 
-    def log(self, message):
-        self._write_to_console(message, "white", "default")
+    def get_path(self):
+        return self.logPath
+
+    def log(self, message, sameLine=False):
+        self._write_to_console(message, "white", "default", sameLine)
         self._write_to_log_file(message, "default")
 
-    def info(self, message):
-        self._write_to_console(message, "blue", "info")
+    def info(self, message, sameLine=False):
+        self._write_to_console(message, "blue", "info", sameLine)
         self._write_to_log_file(message, "info")
 
-    def error(self, message):
-        self._write_to_console(message, "red", "error")
+    def error(self, message, sameLine=False):
+        self._write_to_console(message, "red", "error", sameLine)
         self._write_to_log_file(message, "error")
 
-    def warning(self, message):
-        self._write_to_console(message, "yellow", "warning")
+    def warning(self, message, sameLine=False):
+        self._write_to_console(message, "yellow", "warning", sameLine)
         self._write_to_log_file(message, "warning")
 
-    def success(self, message):
-        self._write_to_console(message, "green", "success")
+    def success(self, message, sameLine=False):
+        self._write_to_console(message, "green", "success", sameLine)
         self._write_to_log_file(message, "success")
 
-    def _write_to_console(self, message, color, log_file):
+    def _write_to_console(self, message, color, log_file, sameLine=False):
         if self.logType == "console" or self.logType == "log":
             timestamp = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
             caller = colored(self.caller, "magenta")
             message = colored(message, color)
-            print(f"[{timestamp}] [{caller}] - {message}")
+            if sameLine:
+                print(f"[{timestamp}] [{caller}] - {message}", end='\r')
+            else:
+                print(f"[{timestamp}] [{caller}] - {message}")
 
     def _write_to_log_file(self, message, log_file):
         if self.logType == "logfile" or self.logType == "log":
